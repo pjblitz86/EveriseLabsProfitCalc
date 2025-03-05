4556b921c04a8ef24d89130e4ce486962cf1719e
pjblitz86 <paulius.juknis@gmail.com>
2025-03-05 08:52:07 +0200

    Trades class and generated data.csv

---
+++ EveriseLabsProfitCalc/Trades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EveriseLabsProfitCalc
+{
+    class Trades
+    {
+        public int TradeId { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Client { get; set; }
+        public string Security { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        public decimal Fee { get; set; }
+    }
+}
